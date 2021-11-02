61852d240b5f7a5f6b16b757e8bfe8006309bfb6
Wael Elk <codewithwael@gmail.com>
2021-11-02 06:24:45 +0000

    retry

--- src/pages/BlogDetails.js
+++ src/pages/BlogDetails.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
-import { Link } from 'react-router-dom'
 import Moment from 'react-moment'
 import ReactMarkdown from 'react-markdown'
 

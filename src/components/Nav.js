a560c4ea1b277a9c7c1aba00634d7d6caeded689
Wael Elk <codewithwael@gmail.com>
2021-11-02 05:40:56 +0000

    logo

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,15 +6,13 @@ function Nav() {
     return (
         <>
         <div className="flex justify-between my-8 mx-10">
-            <h1 className="text-white text-2xl">
-                
-            </h1>
+            <img src="/Images/Hatem.png" className="w-8 h-8 lg:w-12 h-12" />
             <Link to={`/blog`} id="plus" className="cursor-pointer font-bold">
             <h1 className="text-white text-xl lg:text-2xl md:text-2xl">
                 + INFORMATION  
             </h1>
             </Link>
-            </div>
+        </div>
             
             
         </>

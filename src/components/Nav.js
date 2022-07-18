a768b0f45cef5c1910238b2fc29ad60bc08045bd
Wael Elk <73958118+waelelk@users.noreply.github.com>
2022-07-18 03:28:00 +0100

    Grammar mistake

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -20,7 +20,7 @@ export default function Nav() {
                 </Link>
                 <div className="grid grid-cols-2 gap-2 content-center">
                     <button onClick={toggleDarkMode} for="small-toggle" className="pY-3">
-                    <span class="dark:text-white px-3 text-sm font-bold">CHANGE MOOD</span>
+                    <span class="dark:text-white px-3 text-sm font-bold">CHANGE MODE</span>
                     </button>
                     <Link to={`/blog`} id="plus" className="cursor-pointer font-bold ">
                         <h1 className="dark:text-white text-base lg:text-2xl md:text-2xl">

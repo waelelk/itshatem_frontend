a04b5edc432d4fdde409ceb04915e7f758099294
Wael Elk <codewithwael@gmail.com>
2022-09-02 10:09:30 +0700

    Cursor Fixed

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -16,9 +16,9 @@ export default function Nav() {
                             +HATEM
                         </h1>
                 </Link>
-                <div className="grid grid-cols-2 gap-2 content-center">
-                    <button onClick={toggleDarkMode} for="small-toggle" className="">
-                    <span class="dark:text-white px-3 text-sm font-bold">CHANGE MODE</span>
+                <div className="grid grid-cols-2 gap-2 content-center ">
+                    <button onClick={toggleDarkMode}>
+                    <span class="dark:text-white px-3 text-sm font-bold ">CHANGE MODE</span>
                     </button>
                     <Link to={`/blog`} id="plus" className="cursor-pointer font-bold ">
                         <h1 className="dark:text-white text-base lg:text-2xl md:text-2xl">

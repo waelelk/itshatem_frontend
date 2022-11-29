135946dc679a2f87ac12fb2eb77cfde8d26a3609
Wael Elk <73958118+waelelk@users.noreply.github.com>
2022-11-29 13:00:22 +0200

    Update Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -19,12 +19,7 @@ export default function Nav() {
                 <div className="grid grid-cols-2 gap-2 content-center ">
                     <button onClick={toggleDarkMode}>
                     <span class="dark:text-white px-3 text-sm font-bold ">CHANGE MODE</span>
-                    </button>
-                    <Link to={`/blog`} id="plus" className="cursor-pointer font-bold ">
-                        <h1 className="dark:text-white text-base lg:text-2xl md:text-2xl">
-                            + INFORMATION
-                        </h1>
-                    </Link>                                  
+                    </button>                                  
                 </div> 
             </div>
         </div>  
